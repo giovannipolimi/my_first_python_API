aa4a57dcf094764159efb269384935d0854a7d81
https://gitlab.com/palagio <giovanni.palazzo91@gmail.com>
2020-10-17 17:02:35 +0200

    pause

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pandas
+import pandas as pd
 print("hello github")
 
 print("Hello VS Code")
 
 # TODO #1@giovannipolimi
 
+df = pd.read_csv('./data/titanic.csv')
+df.to_json(r'./data/titanic.json')
+
+print(df.head())
